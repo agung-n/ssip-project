ae2d1480371da12f4926e0b5422fbedfe73442c8
agung-n <agung.nugraha@live.com>
2019-12-11 01:29:42 +0700

    Update index.php
    recreating index page to guide visitor

--- index.php
+++ index.php
@@ -1,53 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Welcome to SSIP Final</title>
+  </head>
+  <body>
+    <h1>Welcome to SSIP Final Project</h1>
+    <p>Our group is:</p>
+    <p>Agung Nugraha (012201805040)</p>
+    <p>Alief</p>
+    <p>Yusuf Alexander</p>
 
-    <html>
-        <body>
-
-    <p>
-<?php
-echo "selamat malam";
-echo "<br>";
-$a = 10;
-$b = "selamat malam";
-$c = 30;
-echo $a;
-echo "<br>";
-echo $b;
-echo "<br>";
-
-echo $a.$b;
-
-print $a;
-
-echo "<br><br><br>";
-
-echo "<u>".$a."</u> ini adalah echo var a<br>";
-echo "<u>".$b."</u> ini adalah echo var b<br>";
-print "<u>".$c."</u> ini adalah print var c<br>";
-
-echo var_dump($a);
-
-echo "<br><br><br>";
-
-function lala(){
-  global $a;
-  $a++;
-  echo $a;
-  $a = "bakso";
-  echo $a;
-
-  }
-lala();
-lala();
-lala();
-lala();
-lala();
-lala();
-echo lala();
-
-echo $a;
-echo $b;
-?>
-  </p>
-
-        </body>
-    </html>
+    <a href="form-input.php">Input Student</a><br>
+    <a href="student-list.php">List Student</a>
+  </body>
+</html>
