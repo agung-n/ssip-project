183ef02dd48079c8427b37c341dc15e8c52df110
agung-n <agung.nugraha@live.com>
2019-12-11 12:07:36 +0700

    Update student-list.php
    added gender to display list and add delete and edit button

--- student-list.php
+++ student-list.php
@@ -1,16 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
     include 'connection.php';
 
-$sql = "SELECT studentID, studentName FROM mst_student";
+$sql = "SELECT studentID, studentName, studentGender FROM mst_student";
 $result = $connection->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
+    	echo "<table>";
+    	echo "<tr>";
+		echo "<td>"."Student ID"."</td>";
+		echo "<td>Name</td>";
+		echo "<td>Gender</td>";
+		echo "<td>Delete/Edit</td>";
+		echo "</tr>";
+		
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["studentID"]. " - Name: " . $row["studentName"]."<br>";
+		echo "<tr>";
+		echo "<td>".$row['studentID']."</td>";
+		echo "<td>".$row['studentName']."</td>";
+		echo "<td>".$row['studentGender']."</td>";
+		echo "<td><a href='delete.php?id=".$row['studentID']."'>Delete   </a>"; //if you want to delete based on studentID
+		echo "<a href='edit.php?id=".$row['studentID']."'>   Edit</a>"; //if you want to edit based on studentID
+		echo "</tr>";
+		}
+		echo "</table>";
     }
-} else {
+ else {
     echo "0 results";
 }
 
 ?>
+
+</body>
+</html>
